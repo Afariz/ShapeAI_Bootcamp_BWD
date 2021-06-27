d02eef6b8185691f88270d29f7b86f2337693b2b
Afariz <86526291+Afariz@users.noreply.github.com>
2021-06-27 12:10:51 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>copy right by ShapeAI @ {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>python and c++</h1>
+      <p>basic of python and c++</p>
+    </div>
+  );
+}
+
+export default Info;
